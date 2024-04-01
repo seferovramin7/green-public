392f69084c41129070e1fbfdd8efc55636137a5b
Nicat Qurbanov <nicat.qurbanov97@gmail.com>
2024-04-01 11:53:14 +0400

    - Care product and Store entities with relevant fields are added.

---
+++ src/api/care-product/routes/care-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * care-product router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::care-product.care-product');
