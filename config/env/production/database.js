1e833d8564c632e6fa0923929a2882b72caf2bc5
seferovramin7 <seferovramin7@gmail.com>
2024-04-18 13:55:05 +0400

    env added

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,23 @@
-const path = require('path');
+const { parse } = require('pg-connection-string');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  // Parse Heroku's DATABASE_URL to extract connection details
+  const herokuDbConfig = parse(env('DATABASE_URL'));
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', 'ec2-35-169-9-79.compute-1.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd6aoa6mkf3mdlc'),
-      user: env('DATABASE_USERNAME', 'hxrnzwmdupwxhk'),
-      password: env('DATABASE_PASSWORD', '92d605585cad45f9f1822668be42c455f26b98476733047873b5d427d7ec0086'),
-      ssl: true,
+      client: 'postgres',
+      connection: {
+        host: herokuDbConfig.host,
+        port: herokuDbConfig.port,
+        database: herokuDbConfig.database,
+        user: herokuDbConfig.user,
+        password: herokuDbConfig.password,
+        ssl: {
+          rejectUnauthorized: false, // Heroku requires SSL, set this to false for development
+        },
+      },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};
