bb7f0a9bb77f3d934c86d6900df9658e38c7a7af
Nicat Qurbanov <nicat.qurbanov97@gmail.com>
2024-04-29 10:26:03 +0400

    - planting-process.js is added

---
+++ src/api/planting-process/services/planting-process.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * planting-process service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::planting-process.planting-process');
