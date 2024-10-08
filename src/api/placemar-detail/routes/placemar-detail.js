4da7faebe8bf72d0ffa0b8fc51ce802d39c9bf22
seferovramin7 <seferovramin7@gmail.com>
2024-10-08 16:56:56 +0400

    latest updates

---
+++ src/api/placemar-detail/routes/placemar-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * placemar-detail router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::placemar-detail.placemar-detail');
---
+++ src/api/placemar-detail/services/placemar-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * placemar-detail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::placemar-detail.placemar-detail');
