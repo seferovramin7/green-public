e54f2b8aa9c1e853037dfa126bce32bc586139cc
seferovramin7 <seferovramin7@gmail.com>
2024-10-18 15:29:59 +0400

    Tree update

---
+++ src/api/characteristic-bundle/services/characteristic-bundle.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * characteristic-bundle service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::characteristic-bundle.characteristic-bundle');
---
+++ src/api/characteristic/routes/characteristic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * characteristic router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::characteristic.characteristic');
