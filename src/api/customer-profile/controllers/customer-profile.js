d1e152a6056f4761824c7c7c579948c1d0bdbe38
seferovramin7 <seferovramin7@gmail.com>
2024-10-29 17:00:33 +0400

    Store relations updated

---
+++ src/api/customer-profile/controllers/customer-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::customer-profile.customer-profile');
---
+++ src/api/customer-profile/routes/customer-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-profile router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::customer-profile.customer-profile');
